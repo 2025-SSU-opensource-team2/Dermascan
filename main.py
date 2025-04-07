23485954469ea42b42defeefd9e97d14da09a93f
김도훈 <dhkim7071@naver.com>
2025-04-07 17:29:30 +0900

    feat: 이미지 업로드 폴더 추가

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
 import os
 import shutil
+import io
+import torch
+import torchvision.transforms as transforms
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = torch.load("model.pt", map_location=torch.device("cpu"))
+model.eval()
+
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),   
+    transforms.ToTensor(),         
+    transforms.Normalize([0.5]*3, [0.5]*3)  
+])
 
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
@@ -19,3 +42,17 @@ async def upload_image(file: UploadFile = File(...)):
 def get_file(filename: str):
     file_path = os.path.join(UPLOAD_DIR, filename)
     return FileResponse(file_path)
+
+@app.post("/predict/")
+async def predict_image(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+    input_tensor = transform(image).unsqueeze(0)  
+   
+    with torch.no_grad():
+        output = model(input_tensor)
+        pred = output.argmax(dim=1).item()
+
+  
+    return JSONResponse(content={"result": pred})
